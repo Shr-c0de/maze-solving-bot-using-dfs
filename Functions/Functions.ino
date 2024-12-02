f93bc7cbb199e3a12efefdb692f0cf318c7fc89a
Shreyas <202301245@daiict.ac.in>
2024-12-02 18:47:00 +0530

    etc

--- Functions/Functions.ino
+++ Functions/Functions.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
 
+
+
+class Sensor {
+  int sensors[] = { 4, 5, 6, 13 };
+
+public:
+  Sensor() {
+    for (int i = 0; i < 4; i++) pinMode(sensors[i], OUTPUT);
+  }
+};
+
+
+void setup() {
 }
 
-void loop() {
+void loop(){
   // put your main code here, to run repeatedly:
 
-}
+}[4, 5, 6, 13]
