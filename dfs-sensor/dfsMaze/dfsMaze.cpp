78108e611cf5e5299ec40152b4a4f3b685b246c8
Shr-c0de <202301246@daiict.ac.in>
2024-12-08 19:28:40 +0530

    multi sensor support v1

--- dfs-sensor/dfsMaze/dfsMaze.cpp
+++ dfs-sensor/dfsMaze/dfsMaze.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <vector>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/pio.h"
@@ -25,6 +26,32 @@ void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq)
     pio->txf[sm] = (125000000 / (2 * freq)) - 3;
 }
 
+bool reserved_addr(uint8_t addr)
+{
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+void i2c_scan()
+{
+    printf("  00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f\n");
+    for (int addr = 0; addr < (1 << 7); ++addr)
+    {
+        if (addr % 16 == 0)
+        {
+            printf("%02x ", addr);
+        }
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
+}
+
 int main()
 {
     stdio_init_all();
@@ -36,23 +63,46 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
-    // PIO Blinking example
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &blink_program);
     blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
 
-    VL53L0X s1;
-    s1.init();
-    s1.setTimeout(500);
-    s1.setMeasurementTimingBudget(70000);
-    s1.setSignalRateLimit(0.01);
-    s1.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 18);
-
-    while (true)
+    int xshut[2] = {4, 5};
+    for (int i = 0; i < 2; i++)
     {
-        printf("dist = %d\n", s1.readRangeSingleMillimeters());
+        gpio_init(xshut[i]);
+        gpio_set_dir(xshut[i], GPIO_OUT);
+        gpio_put(xshut[i], 0);
+    }
+    sleep_ms(5000);
+    printf("Program starts:\n\n");
+    i2c_scan();
+
+    VL53L0X sensor[2];
+    uint8_t addr[2] = {0x30, 0x40};
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Sensor %d setup\n", i);
+        gpio_put(xshut[i], 1);
+        sleep_ms(50);
+        VL53L0X tmp;
+        tmp.init();
+        tmp.setTimeout(500);
+        tmp.setMeasurementTimingBudget(70000);
+        tmp.setSignalRateLimit(0.01);
+        tmp.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 18);
+        tmp.setAddress(addr[i]);
+        sensor[i] = tmp;
+        i2c_scan();
+        sleep_ms(500);
+    }
+
+    while (1)
+    {
+        printf("sensor1 value = %d\n sensor2 value = %d\n\n",
+               sensor[0].readRangeSingleMillimeters(), sensor[1].readRangeSingleMillimeters());
         sleep_ms(1000);
     }
 }
