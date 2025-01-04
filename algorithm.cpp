712d0f9b8d9a1e48d047684ee62479b9c3f58dd2
Krutant06 <krutantjethva01@gmail.com>
2025-01-04 21:24:56 +0530

    Update algorithm.cpp
    updated and resolved backtracking using nodes linkedlist

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,52 +1,64 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-class Node {
+class Node
+{
 public:
   int x, y;
   Node(int x = 0, int y = 0)
-    : x(x), y(y) {}
-  bool equals(const Node& other) const {
+      : x(x), y(y) {}
+
+  bool equals(const Node &other) const
+  {
     return x == other.x && y == other.y;
   }
 };
 
-class ListNode {
+class ListNode
+{
 public:
   Node data;
-  ListNode* next;
-  ListNode* prev;
+  ListNode *next;
+  ListNode *prev;
   ListNode(Node node)
-    : data(node), next(nullptr), prev(nullptr) {}
+      : data(node), next(nullptr), prev(nullptr) {}
 };
 
-class LinkedList {
+class LinkedList
+{
 public:
-  ListNode* head;
-  ListNode* tail;
+  ListNode *head;
+  ListNode *tail;
   int elements;
 
   LinkedList()
-    : head(nullptr), tail(nullptr), elements(0) {}
+      : head(nullptr), tail(nullptr), elements(0) {}
 
-  void addEnd(Node node) {
-    ListNode* newNode = new ListNode(node);
-    if (tail == nullptr) {
+  void addEnd(Node node)
+  {
+    ListNode *newNode = new ListNode(node);
+    if (tail == nullptr)
+    {
       head = tail = newNode;
-    } else {
+    }
+    else
+    {
       tail->next = newNode;
       newNode->prev = tail;
       tail = newNode;
     }
   }
 
-  void addpathEnd(Node node) {
-    ListNode* newNode = new ListNode(node);
-    if (tail == nullptr) {
+  void addpathEnd(Node node)
+  {
+    ListNode *newNode = new ListNode(node);
+    if (tail == nullptr)
+    {
       head = tail = newNode;
-    } else {
+    }
+    else
+    {
       tail->next = newNode;
       newNode->prev = tail;
       tail = newNode;
@@ -54,27 +66,38 @@ public:
     elements++;
   }
 
-  void removeEnd() {
-    if (tail != nullptr) {
-      ListNode* temp = tail;
+  void removeEnd()
+  {
+    if (tail != nullptr)
+    {
+      ListNode *temp = tail;
       tail = tail->prev;
-      if (tail != nullptr) {
+      if (tail != nullptr)
+      {
         tail->next = nullptr;
-      } else {
+      }
+      else
+      {
         head = nullptr;
       }
       delete temp;
     }
   }
 
-  void removepathEnd() {
-    if (elements == 1) {
-      if (tail != nullptr) {
-        ListNode* temp = tail;
+  void removepathEnd()
+  {
+    if (elements == 1)
+    {
+      if (tail != nullptr)
+      {
+        ListNode *temp = tail;
         tail = tail->prev;
-        if (tail != nullptr) {
+        if (tail != nullptr)
+        {
           tail->next = nullptr;
-        } else {
+        }
+        else
+        {
           head = nullptr;
         }
         delete temp;
@@ -83,12 +106,17 @@ public:
         Node initial(0, 0);
         path.addpathEnd(initial);
       }
-    } else if (tail != nullptr) {
-      ListNode* temp = tail;
+    }
+    else if (tail != nullptr)
+    {
+      ListNode *temp = tail;
       tail = tail->prev;
-      if (tail != nullptr) {
+      if (tail != nullptr)
+      {
         tail->next = nullptr;
-      } else {
+      }
+      else
+      {
         head = nullptr;
       }
       delete temp;
@@ -96,18 +124,23 @@ public:
     elements--;
   }
 
-  Node getEnd() {
+  Node getEnd()
+  {
     return (tail != nullptr) ? tail->data : Node(-1, -1);
   }
 
-  bool isEmpty() {
+  bool isEmpty()
+  {
     return tail == nullptr;
   }
 
-  bool contains(Node n) {
-    ListNode* current = head;
-    while (current != nullptr) {
-      if (current->data.equals(n)) {
+  bool contains(Node n)
+  {
+    ListNode *current = head;
+    while (current != nullptr)
+    {
+      if (current->data.equals(n))
+      {
         return true;
       }
       current = current->next;
@@ -115,61 +148,123 @@ public:
     return false;
   }
 
-  void printPath() {
-    ListNode* current = head;
-    cout << "Current Path: ";
-    while (current != nullptr) {
+  void printNodes()
+  {
+    ListNode *current = head;
+    cout << "Current Nodes: ";
+    while (current != nullptr)
+    {
       cout << "(" << current->data.x << ", " << current->data.y << ") ";
       current = current->next;
     }
     cout << endl;
   }
+
+  Node getPreviousNode()
+  {
+    if (tail != nullptr && tail->prev != nullptr)
+    {
+      return tail->prev->data;
+    }
+    return Node(-1, -1);
+  }
 };
 
-string TurnLeft(string dir) {
-  if (dir == "N") return "W";
-  if (dir == "W") return "S";
-  if (dir == "S") return "E";
+string TurnLeft(string dir)
+{
+  if (dir == "N")
+    return "W";
+  if (dir == "W")
+    return "S";
+  if (dir == "S")
+    return "E";
   return "N";
 }
 
-string Uturn(string dir) {
-  if (dir == "N") return "S";
-  if (dir == "W") return "E";
-  if (dir == "S") return "N";
+string Uturn(string dir)
+{
+  if (dir == "N")
+    return "S";
+  if (dir == "W")
+    return "E";
+  if (dir == "S")
+    return "N";
   return "W";
 }
 
-string TurnRight(string dir) {
-  if (dir == "N") return "E";
-  if (dir == "E") return "S";
-  if (dir == "S") return "W";
+string TurnRight(string dir)
+{
+  if (dir == "N")
+    return "E";
+  if (dir == "E")
+    return "S";
+  if (dir == "S")
+    return "W";
   return "N";
 }
 
-Node MoveForward(int& x, int& y, string direction) {
-  if (direction == "N") y++;
-  else if (direction == "S") y--;
-  else if (direction == "E") x++;
-  else if (direction == "W") x--;
+Node MoveForward(int &x, int &y, string direction)
+{
+  if (direction == "N")
+    y++;
+  else if (direction == "S")
+    y--;
+  else if (direction == "E")
+    x++;
+  else if (direction == "W")
+    x--;
 
   return Node(x, y);
 }
 
-LinkedList path;         
-LinkedList visited;      
-string direction = "N";  
+int checknodes(int frontdistance, int leftdistance, int rightdistance)
+{
+  int count = 0;
+  if (frontdistance > 15)
+  {
+    count++;
+  }
+  if (leftdistance > 15)
+  {
+    count++;
+  }
+  if (rightdistance > 15)
+  {
+    count++;
+  }
+
+  if (count > 1)
+  {
+    cout << "It is a node" << endl;
+    return 1;
+  }
+  return 0;
+}
+
+LinkedList path;
+LinkedList visited;
+LinkedList nodes;
+
+string direction = "N";
 double distances[4];
 
-void DFS() {
+void DFS()
+{
   int x = 0, y = 0;
   Node start(x, y);
   path.addpathEnd(start);
   visited.addEnd(start);
+  nodes.addEnd(start);
+  int flag = 0;
 
-  while (true) {
+  while (true)
+  {
 
-    path.printPath();
+    int backtrackingSteps = 0;
+    const int MAX_BACKTRACKING_STEPS = 100;
+
+    path.printNodes();
+    nodes.printNodes();
 
     cout << "Enter front1 distance: ";
     cin >> distances[0];
@@ -180,8 +275,9 @@ void DFS() {
     cout << "Enter right distance: ";
     cin >> distances[3];
 
-    if (distances[0] == -1 && distances[1] == -1 && distances[2] == -1 && distances[3] == -1) {
-      cout << "Exiting program as per user input." << endl;
+    if (distances[0] == -1 && distances[1] == -1 && distances[2] == -1 && distances[3] == -1)
+    {
+      cout << "Solved!" << endl;
       break;
     }
 
@@ -192,170 +288,251 @@ void DFS() {
 
     int frontDistance = (front1Distance + front2Distance) / 2;
 
-    Node next_pos(x, y);
-    if (direction == "N") next_pos.y++;
-    else if (direction == "S") next_pos.y--;
-    else if (direction == "E") next_pos.x++;
-    else if (direction == "W") next_pos.x--;
+    Node front_pos(x, y);
+    if (direction == "N")
+      front_pos.y++;
+    else if (direction == "S")
+      front_pos.y--;
+    else if (direction == "E")
+      front_pos.x++;
+    else if (direction == "W")
+      front_pos.x--;
 
-    if (frontDistance > 15 && !visited.contains(next_pos))
+    Node left_pos(x, y);
+    if (direction == "N")
+      left_pos.x--;
+    else if (direction == "S")
+      left_pos.x++;
+    else if (direction == "E")
+      left_pos.y++;
+    else if (direction == "W")
+      left_pos.y--;
+
+    Node right_pos(x, y);
+    if (direction == "N")
+      right_pos.x++;
+    else if (direction == "S")
+      right_pos.x--;
+    else if (direction == "E")
+      right_pos.y--;
+    else if (direction == "W")
+      right_pos.y++;
+
+    Node back_pos(x, y);
+    if (direction == "N")
+      back_pos.y--;
+    else if (direction == "S")
+      back_pos.y++;
+    else if (direction == "E")
+      back_pos.x--;
+    else if (direction == "W")
+      back_pos.x++;
+
+    Node current_pos(x, y);
+
+    if (!flag)
     {
+
+      int is_node = checknodes(frontDistance, leftDistance, rightDistance);
+      if (is_node)
+      {
+        nodes.addEnd(current_pos);
+      }
+    }
+
+    if (frontDistance > 15 && !visited.contains(front_pos))
+    {
+      flag = 0;
       MoveForward(x, y, direction);
-      path.addpathEnd(next_pos);
-      visited.addEnd(next_pos);
-      cout<<"Moving forward"<<endl;
-    } 
+      path.addpathEnd(front_pos);
+      visited.addEnd(front_pos);
 
-    else if (leftDistance > 15) 
+      cout << "Moving forward" << endl;
+    }
+
+    else if (leftDistance > 15 && !visited.contains(left_pos))
     {
+      flag = 1;
       direction = TurnLeft(direction);
-      cout<< "Turning Left"<<endl;
-    } 
-    
-    else if (rightDistance > 15) 
+      cout << "Turning Left" << endl;
+    }
+
+    else if (rightDistance > 15 && !visited.contains(right_pos))
     {
+      flag = 1;
       direction = TurnRight(direction);
-      cout << "Turning Right" <<endl;
-    } 
+      cout << "Turning Right" << endl;
+    }
 
-    else 
+    else
     {
-        cout<<"Getting into backtracking"<<endl;
-        int flag = 1;
-        while (true) {
 
-        if (flag)
+      cout << "Getting into backtracking" << endl;
+
+      Node targetNode;
+
+      int is_a_node = checknodes(frontDistance, leftDistance, rightDistance);
+
+      if (is_a_node)
+      {
+        targetNode = nodes.getPreviousNode();
+      }
+      else
+      {
+        targetNode = nodes.getEnd();
+      }
+
+      while (true)
+      {
+        backtrackingSteps++;
+        if (backtrackingSteps > MAX_BACKTRACKING_STEPS)
         {
-            direction = Uturn(direction);
-            cout<<"Taking a Uturn"<<endl;
-            flag =0;
+          cout << " Backtracking exceeded step limit." << endl;
+          break;
+        }
+
+        Node currentNode = path.getEnd();
+        x = currentNode.x;
+        y = currentNode.y;
+        Node previousNode = path.getPreviousNode();
+
+        front_pos = Node(x, y);
+        if (direction == "N")
+          front_pos.y++;
+        else if (direction == "S")
+          front_pos.y--;
+        else if (direction == "E")
+          front_pos.x++;
+        else if (direction == "W")
+          front_pos.x--;
+
+        left_pos = Node(x, y);
+        if (direction == "N")
+          left_pos.x--;
+        else if (direction == "S")
+          left_pos.x++;
+        else if (direction == "E")
+          left_pos.y++;
+        else if (direction == "W")
+          left_pos.y--;
+
+        right_pos = Node(x, y);
+        if (direction == "N")
+          right_pos.x++;
+        else if (direction == "S")
+          right_pos.x--;
+        else if (direction == "E")
+          right_pos.y--;
+        else if (direction == "W")
+          right_pos.y++;
+
+        back_pos = Node(x, y);
+        if (direction == "N")
+          back_pos.y--;
+        else if (direction == "S")
+          back_pos.y++;
+        else if (direction == "E")
+          back_pos.x--;
+        else if (direction == "W")
+          back_pos.x++;
+
+        path.printNodes();
+        nodes.printNodes();
+
+        if (currentNode.equals(targetNode))
+        {
+          cout << "Reached the target node during backtracking" << endl;
+          nodes.removeEnd();
+          flag = 0;
+          break;
+        }
+
+        else if (front_pos.equals(previousNode))
+        {
+          MoveForward(x, y, direction);
+          path.removepathEnd();
+          cout << "Moving forward to the previous node" << endl;
+        }
+
+        else if (back_pos.equals(previousNode))
+        {
+          direction = Uturn(direction);
+          MoveForward(x, y, direction);
+          path.removepathEnd();
+          cout << "Taking a U-turn and moving to previous node" << endl;
+        }
+
+        else if (left_pos.equals(previousNode))
+        {
+          direction = TurnLeft(direction);
+          cout << "Turning Left" << endl;
+        }
+
+        else if (right_pos.equals(previousNode))
+        {
+          direction = TurnRight(direction);
+          cout << "Turning Right" << endl;
         }
         
-        path.printPath(); 
-
-        cout << "Enter front1 distance: ";
-        cin >> distances[0];
-        cout << "Enter front2 distance: ";
-        cin >> distances[1];
-        cout << "Enter left distance: ";
-        cin >> distances[2];
-        cout << "Enter right distance: ";
-        cin >> distances[3];
-
-        if (distances[0] == -1 && distances[1] == -1 && distances[2] == -1 && distances[3] == -1) {
-          cout << "Exiting program as per user input." << endl;
-          return;
+        else
+        {
+          cout << "Error in backtracking\n";
         }
-
-        int front1Distance = distances[0];
-        int front2Distance = distances[1];
-        int leftDistance = distances[2];
-        int rightDistance = distances[3];
-
-        int frontDistance = (front1Distance + front2Distance)/2;
-
-
-        Node right(x, y);
-        if (direction == "S") right.x--;
-        else if (direction == "N") right.x++;
-        else if (direction == "W") right.y++;
-        else right.y--;
-
-        Node left(x, y);
-        if (direction == "S") left.x++;
-        else if (direction == "N") left.x--;
-        else if (direction == "W") left.y--;
-        else left.y++;
-
-        Node front(x, y);
-        if (direction == "S") front.y--;
-        else if (direction == "N") front.y++;
-        else if (direction == "W") front.x--;
-        else front.x++;
-
-
-        bool checkF = (frontDistance > 15) && path.contains(front) && (front.y >= 0);
-        bool checkL = (leftDistance > 15) && path.contains(left) && (left.y >= 0);
-        bool checkR = (rightDistance > 15) && path.contains(right) && (right.y >= 0);
-
-        if(frontDistance > 15 && !visited.contains(front)){
-            cout<<"Getting out of backtracking and returning to DFS"<<endl;
-            MoveForward(x,y,direction);
-            cout<<"Moving forward"<<endl;
-            path.addEnd(front);
-            visited.addEnd(front);
-            break;
+        if (path.isEmpty() || nodes.isEmpty())
+        {
+          cout << " Backtracking terminated. Path or Nodes list is empty." << endl;
+          break;
         }
-        else if(leftDistance > 15 && !visited.contains(left)){
-            direction = TurnLeft(direction);
-            cout<<"Turning Left"<<endl;
-        }
-
-        else if(rightDistance > 15 && !visited.contains(right)){
-            direction = TurnRight(direction);
-            cout<<"Turning Right"<<endl;
-        }
-
-
-        else if (!(checkF || checkL || checkR)) {
-            direction = Uturn(direction);
-        }
-
-        else if (checkF) {
-            MoveForward(x, y, direction);
-            path.removepathEnd();
-        }
-
-        else if (checkL) {
-            direction = TurnLeft(direction);
-            cout<<"Turning Left"<<endl;
-        }
-
-        else if (checkR) {
-            direction = TurnRight(direction);
-            cout<<"Turning Right"<<endl;
-        }
-
       }
     }
   }
 }
 
-
-class ActionNode {
+class ActionNode
+{
 public:
-  string action; 
-  int steps;     
-  ActionNode* next;
+  string action;
+  int steps;
+  ActionNode *next;
 
   ActionNode(string action, int steps = 0)
-    : action(action), steps(steps), next(nullptr) {}
+      : action(action), steps(steps), next(nullptr) {}
 };
 
-class ActionList {
+class ActionList
+{
 public:
-  ActionNode* head;
-  ActionNode* tail;
+  ActionNode *head;
+  ActionNode *tail;
 
   ActionList() : head(nullptr), tail(nullptr) {}
 
-  void addAction(string action, int steps = 0) {
-    ActionNode* newNode = new ActionNode(action, steps);
-    if (tail == nullptr) {
+  void addAction(string action, int steps = 0)
+  {
+    ActionNode *newNode = new ActionNode(action, steps);
+    if (tail == nullptr)
+    {
       head = tail = newNode;
-    } else {
+    }
+    else
+    {
       tail->next = newNode;
       tail = newNode;
     }
   }
 
-  void printActions() {
-    ActionNode* current = head;
+  void printActions()
+  {
+    ActionNode *current = head;
     cout << "Final route: " << endl;
-    while (current != nullptr) {
-      if (current->action == "Move Forward") {
+    while (current != nullptr)
+    {
+      if (current->action == "Move Forward")
+      {
         cout << current->action << " " << current->steps << " steps" << endl;
-      } else {
+      }
+      else
+      {
         cout << current->action << endl;
       }
       current = current->next;
@@ -363,44 +540,51 @@ public:
   }
 };
 
-ActionList finalpath(LinkedList& path) {
+ActionList finalpath(LinkedList &path)
+{
   ActionList actions;
-  
-  ListNode* current = path.head;
+
+  ListNode *current = path.head;
   string currentDirection = "N";
   int stepCount = 0;
 
-  while (current->next != nullptr) {
+  while (current->next != nullptr)
+  {
     Node currNode = current->data;
     Node nextNode = current->next->data;
 
     string newDirection;
-    if (nextNode.x > currNode.x) newDirection = "E";
-    else if (nextNode.x < currNode.x) newDirection = "W";
-    else if (nextNode.y > currNode.y) newDirection = "N";
-    else if (nextNode.y < currNode.y) newDirection = "S";
+    if (nextNode.x > currNode.x)
+      newDirection = "E";
+    else if (nextNode.x < currNode.x)
+      newDirection = "W";
+    else if (nextNode.y > currNode.y)
+      newDirection = "N";
+    else if (nextNode.y < currNode.y)
+      newDirection = "S";
 
-    if (newDirection == currentDirection) {
+    if (newDirection == currentDirection)
+    {
 
       stepCount++;
-    } 
-    else 
+    }
+    else
     {
-      
-      if (stepCount > 0) 
+
+      if (stepCount > 0)
       {
         actions.addAction("Move Forward", stepCount);
       }
 
-      if ((currentDirection == "N" && newDirection == "E") || (currentDirection == "E" && newDirection == "S") || (currentDirection == "S" && newDirection == "W") || (currentDirection == "W" && newDirection == "N")) 
+      if ((currentDirection == "N" && newDirection == "E") || (currentDirection == "E" && newDirection == "S") || (currentDirection == "S" && newDirection == "W") || (currentDirection == "W" && newDirection == "N"))
       {
         actions.addAction("Turn Right");
-      } 
-      else if ((currentDirection == "N" && newDirection == "W") || (currentDirection == "W" && newDirection == "S") ||(currentDirection == "S" && newDirection == "E") || (currentDirection == "E" && newDirection == "N")) 
+      }
+      else if ((currentDirection == "N" && newDirection == "W") || (currentDirection == "W" && newDirection == "S") || (currentDirection == "S" && newDirection == "E") || (currentDirection == "E" && newDirection == "N"))
       {
         actions.addAction("Turn Left");
-      } 
-      else 
+      }
+      else
       {
         actions.addAction("U-Turn");
       }
@@ -412,16 +596,17 @@ ActionList finalpath(LinkedList& path) {
     current = current->next;
   }
 
-  if (stepCount > 0) {
+  if (stepCount > 0)
+  {
     actions.addAction("Move Forward", stepCount);
   }
 
   return actions;
 }
 
-
-int main() {
-  DFS(); 
+int main()
+{
+  DFS();
 
   ActionList actions = finalpath(path);
 
@@ -429,4 +614,3 @@ int main() {
 
   return 0;
 }
-
